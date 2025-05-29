127f3ed88556b6d06b94bfbb8fc62d9ac722462c
reichstan <reichstan@aol.com>
2025-05-30 02:48:24 +0530

    Update test_token.py

--- test_token.py
+++ test_token.py
@@ -1,2 +1,5 @@
-# SAFE TEST - NOT A REAL TOKEN
-DISCORD_BOT_TOKEN = "mfa.this_is_FAKE_12345"
+# Discord
+DISCORD_TEST = "mfa.dv_thisIsAFakeToken123456789"
+
+# AWS (will trigger even in private repos)
+AWS_TEST = "AKIAXXXXXXXXXXXXXXXX"
